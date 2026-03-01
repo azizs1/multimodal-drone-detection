a238a1465533bc47a6ceb5313dd9e7799841c0ef
Aziz Shaik <azizs@vt.edu>
2026-03-01 12:30:04 -0500

    add to elements

--- jetson/src/sensor_ingestion/ingest_gi.py
+++ jetson/src/sensor_ingestion/ingest_gi.py
@@ -103,7 +103,7 @@ def build_gst_pipeline():
     thermal_caps = Gst.ElementFactory.make("capsfilter", "thermal_caps")
     thermal_caps.set_property("caps", Gst.Caps.from_string("video/x-raw,width=720,height=256,framerate=30/1"))
     thermal_conv = Gst.ElementFactory.make("nvvidconv", "thermal_conv") # convert to NV12+NVMM
-    thermal_conv.set_property("format", Gst.VideoFormat.NV12)
+    thermal_conv.set_property("format", "NV12")
     # thermal_conv = Gst.ElementFactory.make("videoconvert", "thermal_conv") # convert to NV12+NVMM
     # thermal_caps_nv12 = Gst.ElementFactory.make("capsfilter", "thermal_caps_nv12")
     # thermal_caps_nv12.set_property("caps", Gst.Caps.from_string("video/x-raw,format=NV12"))
@@ -128,7 +128,7 @@ def build_gst_pipeline():
     thermal_rtp_queue.set_property("max-size-buffers", 5)
     thermal_rtp_queue.set_property("leaky", 2)  # 2 means downstream
     thermal_rtp_nvconv = Gst.ElementFactory.make("nvvidconv", "thermal_rtp_nvconv")
-    thermal_rtp_nvconv.set_property("format", Gst.VideoFormat.NV12)
+    thermal_rtp_nvconv.set_property("format", "NV12")
     thermal_rtp_caps = Gst.ElementFactory.make("capsfilter", "thermal_rtp_caps")
     thermal_rtp_caps.set_property("caps", Gst.Caps.from_string("video/x-raw(memory:NVMM),format=NV12"))
     thermal_encoder = Gst.ElementFactory.make("nvv4l2h264enc", "thermal_encoder") # H.264 encoder
@@ -153,7 +153,7 @@ def build_gst_pipeline():
         rgb_appsink, rgb_rtp_queue, rgb_encoder, rgb_rtp_payload, rgb_udpsink,
         thermal_src, thermal_caps, thermal_conv, thermal_nvmm_caps, thermal_tee,
         thermal_inf_queue, thermal_inf_conv, thermal_inf_caps, thermal_appsink,
-        thermal_rtp_queue, thermal_encoder, thermal_rtp_payload, thermal_udpsink
+        thermal_rtp_queue, thermal_rtp_nvconv, thermal_rtp_caps, thermal_encoder, thermal_rtp_payload, thermal_udpsink
     ]
 
     # Add all of the elements to the pipeline
