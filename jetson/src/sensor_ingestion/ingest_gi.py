21c57ded4b42d6aaf743b25fcd56f27d0b521b4f
Chien <chia880507@MacBook-Air.local>
2026-04-27 16:34:22 -0400

    combine jetson containers into one & some ingestion-inference integration stuffs

--- jetson/src/sensor_ingestion/ingest_gi.py
+++ jetson/src/sensor_ingestion/ingest_gi.py
@@ -32,11 +32,7 @@ context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.set(zmq.SNDHWM, 1)  # keep only 1 frame in queue to avoid lag
 socket.setsockopt(zmq.LINGER, 0)
-<<<<<<< HEAD
-socket.bind("ipc:///tmp/frames_bus")
-=======
 socket.bind("ipc:///tmp/frame_bus")
->>>>>>> ccbbf93cf6426ac78078fa90df98005475e855d5
 
 latest_rgb = None
 latest_thermal = None
@@ -105,11 +101,7 @@ def build_gst_pipeline():
     rgb_appsink.set_property("max-buffers", 1)
     rgb_appsink.set_property("drop", True)
 
-<<<<<<< HEAD
-    # rgb to backend (live stream)
-=======
     # rgb to webrtc (live stream)
->>>>>>> ccbbf93cf6426ac78078fa90df98005475e855d5
     rgb_rtp_queue = Gst.ElementFactory.make("queue", "rgb_rtp_queue")
     rgb_rtp_convert = Gst.ElementFactory.make("videoconvert", "rgb_rtp_convert")
 
@@ -159,11 +151,7 @@ def build_gst_pipeline():
     thermal_appsink.set_property("max-buffers", 1)
     thermal_appsink.set_property("drop", True)
 
-<<<<<<< HEAD
-    # thermal to backend (live stream)
-=======
     # thermal to webrtc (live stream)
->>>>>>> ccbbf93cf6426ac78078fa90df98005475e855d5
     thermal_rtp_queue = Gst.ElementFactory.make("queue", "thermal_rtp_queue")
     thermal_rtp_convert = Gst.ElementFactory.make("videoconvert", "thermal_rtp_convert")
 
@@ -177,11 +165,7 @@ def build_gst_pipeline():
     thermal_encoder.set_property("byte-stream", True)
     thermal_encoder.set_property("aud", True)
 
-<<<<<<< HEAD
-    # NEW: MPEG-TS mux instead of RTP payloader
-=======
     # mpeg-ts mux instead of rtp payloader
->>>>>>> ccbbf93cf6426ac78078fa90df98005475e855d5
     thermal_ts_mux = Gst.ElementFactory.make("mpegtsmux", "thermal_ts_mux")
     thermal_ts_mux.set_property("alignment", 7)  # to help with mediamtx latency
 
@@ -241,11 +225,7 @@ def build_gst_pipeline():
     rgb_inf_convert.link(rgb_inf_bgr_caps)
     rgb_inf_bgr_caps.link(rgb_appsink)
 
-<<<<<<< HEAD
-    # rgb to backend
-=======
     # rgb to webrtc
->>>>>>> ccbbf93cf6426ac78078fa90df98005475e855d5
     rgb_tee.link(rgb_rtp_queue)
     rgb_rtp_queue.link(rgb_rtp_convert)
     rgb_rtp_convert.link(rgb_encoder)
@@ -262,11 +242,7 @@ def build_gst_pipeline():
     thermal_inf_convert.link(thermal_inf_bgr_caps)
     thermal_inf_bgr_caps.link(thermal_appsink)
 
-<<<<<<< HEAD
-    # thermal to backend
-=======
     # thermal to webrtc
->>>>>>> ccbbf93cf6426ac78078fa90df98005475e855d5
     thermal_tee.link(thermal_rtp_queue)
     thermal_rtp_queue.link(thermal_rtp_convert)
     thermal_rtp_convert.link(thermal_encoder)
